bcad2c9a847dc719e78b8c84dd79d02478900f95
mrchepe09 <khanujabhupinder09@gmail.com>
2024-07-11 03:44:51 +0530

    fixes

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -30,7 +30,6 @@ export default function Login() {
       }, 1500)
     } finally {
       setIsLoading(false);
-      router.push('/admin')
     }
   }
 
--- src/middleware.ts
+++ src/middleware.ts
@@ -2,36 +2,33 @@ import type { NextRequest } from 'next/server'
 import {jwtVerify} from 'jose';
  
 export async function middleware(request: NextRequest) {
-  // const token = request.cookies.get('token')?.value;
-  // console.log('abc', token, request.nextUrl.pathname);
-  // if(!token && !(request.nextUrl.pathname === '/')){
-  //   return Response.redirect(new URL('/', request.url));
-  // }
-  // if(!token){
-  //   return;
-  // }
-  // try {
-  //   const JWT_SECRET = new TextEncoder().encode(process.env.JWT_SECRET);
-  //   const decoded = await jwtVerify(token, JWT_SECRET);
-  
-  //   const user_type = decoded.payload.type;
+  const token = request.cookies.get('token')?.value;
+  console.log('abc', token, request.nextUrl.pathname);
+  if(token){
+    try {
+      const JWT_SECRET = new TextEncoder().encode(process.env.JWT_SECRET);
+      const decoded = await jwtVerify(token, JWT_SECRET);
+    
+      const user_type = decoded.payload.type;
 
-  //   if(user_type === 'admin' && !(request.nextUrl.pathname === '/admin')){
-  //     return Response.redirect(new URL('/admin', request.url));
-  //   }
-  //   if(user_type === 'user' && !(request.nextUrl.pathname === '/user')){
-  //     return Response.redirect(new URL('/user', request.url));
-  //   }
-  // } catch (err) {
-  //   if(!(request.nextUrl.pathname === '/')){
-  //     return Response.redirect(new URL('/', request.url));
-  //   }
-  // }
-  // if(token && !request.nextUrl.pathname.startsWith('/user')){
-  //   return Response.redirect(new URL('/user', request.url));
-  // }
+      if(user_type === 'admin' && !(request.nextUrl.pathname === '/admin')){
+        return Response.redirect(new URL('/admin', request.url));
+      }
+      if(user_type === 'user' && !(request.nextUrl.pathname === '/user')){
+        return Response.redirect(new URL('/user', request.url));
+      }
+    } catch (err) {
+      if(!(request.nextUrl.pathname === '/')){
+        return Response.redirect(new URL('/', request.url));
+      }
+    }
+  } else {
+    if(!(request.nextUrl.pathname === '/')){
+      return Response.redirect(new URL('/', request.url));
+    }
+  }
 }
- 
+
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+  matcher: ['/', '/user', '/admin'],
 }
