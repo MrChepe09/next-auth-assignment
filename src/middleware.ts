ac96aca65a2ee95d69e049d549d8b1a1129812e7
mrchepe09 <khanujabhupinder09@gmail.com>
2024-07-11 03:30:30 +0530

    removed middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,31 +2,31 @@ import type { NextRequest } from 'next/server'
 import {jwtVerify} from 'jose';
  
 export async function middleware(request: NextRequest) {
-  const token = request.cookies.get('token')?.value;
-  console.log('abc', token, request.nextUrl.pathname);
-  if(!token && !(request.nextUrl.pathname === '/')){
-    return Response.redirect(new URL('/', request.url));
-  }
-  if(!token){
-    return;
-  }
-  try {
-    const JWT_SECRET = new TextEncoder().encode(process.env.JWT_SECRET);
-    const decoded = await jwtVerify(token, JWT_SECRET);
+  // const token = request.cookies.get('token')?.value;
+  // console.log('abc', token, request.nextUrl.pathname);
+  // if(!token && !(request.nextUrl.pathname === '/')){
+  //   return Response.redirect(new URL('/', request.url));
+  // }
+  // if(!token){
+  //   return;
+  // }
+  // try {
+  //   const JWT_SECRET = new TextEncoder().encode(process.env.JWT_SECRET);
+  //   const decoded = await jwtVerify(token, JWT_SECRET);
   
-    const user_type = decoded.payload.type;
+  //   const user_type = decoded.payload.type;
 
-    if(user_type === 'admin' && !(request.nextUrl.pathname === '/admin')){
-      return Response.redirect(new URL('/admin', request.url));
-    }
-    if(user_type === 'user' && !(request.nextUrl.pathname === '/user')){
-      return Response.redirect(new URL('/user', request.url));
-    }
-  } catch (err) {
-    if(!(request.nextUrl.pathname === '/')){
-      return Response.redirect(new URL('/', request.url));
-    }
-  }
+  //   if(user_type === 'admin' && !(request.nextUrl.pathname === '/admin')){
+  //     return Response.redirect(new URL('/admin', request.url));
+  //   }
+  //   if(user_type === 'user' && !(request.nextUrl.pathname === '/user')){
+  //     return Response.redirect(new URL('/user', request.url));
+  //   }
+  // } catch (err) {
+  //   if(!(request.nextUrl.pathname === '/')){
+  //     return Response.redirect(new URL('/', request.url));
+  //   }
+  // }
   // if(token && !request.nextUrl.pathname.startsWith('/user')){
   //   return Response.redirect(new URL('/user', request.url));
   // }
